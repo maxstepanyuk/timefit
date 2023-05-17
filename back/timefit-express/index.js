c480ca46b79f825084f58adce58ca491d43512c1
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2023-05-18 01:11:38 +0300

    index.js comments

--- back/timefit-express/index.js
+++ back/timefit-express/index.js
@@ -1,11 +1,15 @@
-import express from "express";
+import express from 'express';
 
-const app = express();
+const app = express(); // Creating an instance of the express application
 
+app.use(express.json()) // Middleware to parse incoming JSON data
+
+// Handling GET request to the root URL ('/')
 app.get('/', (req,res) =>{
     res.send('send help...');
 })
 
+// Starting the server on port 4444 and handling any potential errors
 app.listen(4444, (err)=>{
     if(err){
         return console.log(err);
