d502a7171365be0d820a176589611d7ccd299ec8
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2023-06-02 19:54:38 +0300

    refactor - beutify

--- back/timefit-express/index.js
+++ back/timefit-express/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 import { registrationValidation } from './validations/registrationValidation.js'
 
-import  authMiddleware from './middleware/authMiddleware.js'
+import authMiddleware from './middleware/authMiddleware.js'
 
 import * as UserController from './controllers/userController.js'
 
@@ -14,25 +14,18 @@ const USER = process.env.USER
 const PASS = process.env.PASS
 
 mongoose.connect(`mongodb+srv://${USER}:${PASS}@cluster0.r1umkoa.mongodb.net/timefit?retryWrites=true&w=majority`)
-.then(() => console.log('mongodb ok'))
-.catch((err) => console.log('mongodb error', err));
+    .then(() => console.log('mongodb ok'))
+    .catch((err) => console.log('mongodb error', err));
 
 const app = express(); // Creating an instance of the express application
 
 app.use(express.json()) // Middleware to parse incoming JSON data
 
-// Handling GET request to the root URL ('/')
-app.get('/', (req, res) => {res.send('OK');});
-
-//reg post
+// Handling requests
+app.get('/', (req, res) => { res.send('OK'); });
 app.post('/auth/registration', registrationValidation, UserController.registration);
-
-//login post
 app.post('/auth/login', UserController.login);
-
-//login post
 app.get('/auth/myinfo', authMiddleware, UserController.myinfo);
-
 
 // Starting the server on port 4444 and handling any potential errors
 app.listen(PORT, (err) => {
