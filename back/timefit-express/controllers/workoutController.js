91959bc1b28adaa33dd5769bfc75aa5c8ae0ef14
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2023-06-05 18:12:59 +0300

    added workout controller

---
+++ back/timefit-express/controllers/workoutController.js
@@ -0,0 +1,29 @@
+import Workout from './../models/Workout.js';
+import { validationResult } from 'express-validator';
+
+export const createWorkout = async (req, res) => {
+    try {
+        // Check for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // Create a new workout based on the validated request body
+        // "user: req.body.user.id" comes from "middleware/authMiddleware.js"
+        const workoutDoc = new Workout({
+            workoutName: req.body.workoutName,
+            grups: req.body.grups,
+            user: req.body.user.id
+        });
+        // workout.user = req.userId
+
+        // Save the workout to the database
+        const savedWorkout = await workoutDoc.save();
+
+        res.status(201).json(savedWorkout);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Failed to create workout' });
+    }
+};
