a6732cfe45bcdad1042cf004862d92a3f151a5d9
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2023-06-08 13:38:50 +0300

    setup my structure

--- front/timefit-react/src/index.js
+++ front/timefit-react/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/reset-meyerweb.css';
+import './styles/reset.css';
+// import './styles/test.css';
+import './styles/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
